5a70a85fc94a0fa49831ed2ab4af742621522995
douc <espoirimmo9@gmail.com>
2024-08-02 03:04:11 +0000

    restau commit

---
+++ src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import AboutBackground from "../Assets/about-background.png";
+import AboutBackgroundImage from "../Assets/about-background-image.png";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+
+const About = () => {
+  return (
+    <div className="about-section-container">
+      <div className="about-background-image-container">
+        <img src={AboutBackground} alt="" />
+      </div>
+      <div className="about-section-image-container">
+        <img src={AboutBackgroundImage} alt="" />
+      </div>
+      <div className="about-section-text-container">
+        <p className="primary-subheading">About</p>
+        <h1 className="primary-heading">
+          DOUCOURE  RESTAURANT A BAMAKO
+        </h1>
+        <p className="primary-text">
+          Bienvenu dans le restaurant DOUCOURE-FOOD
+        </p>
+        <p className="primary-text">
+          Non tincidunt magna non et elit. Dolor turpis molestie dui magnis
+          facilisis at fringilla quam.
+        </p>
+        <div className="about-buttons-container">
+          <button className="secondary-button">Lire Plus</button>
+          <button className="watch-video-button">
+            <BsFillPlayCircleFill /> Regarder la Video
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
